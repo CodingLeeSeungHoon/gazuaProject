4fbbad89e4fa8f367623ae7161827239bf4236d9
CodingLeeSeungHoon <free_minkya@naver.com>
2021-03-12 22:02:04 +0900

    avoid bot detection : random sleep, random user-agent testing

--- crawlsite.py
+++ crawlsite.py
@@ -1,38 +1,72 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import numpy as np
 
 """
 2021.03.11
 - 비동기식 Ajax 통신 URL 캐치 성공. 공시 정보 URL 캐치 후 JSON Data 추출 성공
 - json_file 접근법 주석 추가
-- 시간(5초)에 따라 새로운 공시 업데이트 유무 확인
+2021.03.12
+- avoid bot detection : random sleep, random user-agent
+- 
 """
 
-url = 'https://project-team.upbit.com/api/v1/disclosure?region=kr&per_page=20'
+url = 'https://project-team.upbit.com/api/v1/disclosure?region=kr&per_page=1'
 
 
 def _text_to_json(text):
     return json.loads(text)
 
 
+def _is_successful_connected(status_code):
+    if status_code // 100 == 2:
+        return True
+    else:
+        return False
+
+
+def _get_random_ua():
+    random_ua = ''
+    ua_file = 'ua_file.txt'
+
+    try:
+        with open(ua_file) as f:
+            lines = f.readlines()
+        if len(lines) > 0:
+            prng = np.random.RandomState()
+            index = prng.permutation(len(lines) - 1)
+            idx = np.asarray(index, dtype=np.integer)[0]
+            random_ua = lines[int(idx)]
+
+    except Exception as ex:
+        print('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua
+
+
 def get_recent_announced_coin():
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, features="html.parser")
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+    resp = requests.get(url, headers=headers)
+    print(resp.status_code)
+    if _is_successful_connected(resp.status_code):
+        soup = BeautifulSoup(resp.text, features="html.parser")
+        json_file = _text_to_json(soup.text)
 
-    json_file = _text_to_json(soup.text)
+        """
+        json_file['data']['posts'][number][parameter]
+        number :: 0 (url per_page=1)
+        parameter ::
+        - assets : coin name
+        - start_date : "yyyy-mm-ddT00:00:00+09:00"
+        - text : announce title
+        """
 
-    """
-    json_file['data']['posts'][number][parameter]
-    number :: 0~19 (url per_page=20)
-    parameter ::
-    - assets : coin name
-    - start_date : "yyyy-mm-ddT00:00:00+09:00"
-    - text : announce title
-    """
-    return json_file['data']['posts'][0]['assets'], json_file['data']['posts'][0]['text']
+        return json_file['data']['posts'][0]['assets'], json_file['data']['posts'][0]['text']
+
+    return -1, -1
 
 
-def init_db():
-    pass
+
 
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import crawlsite
 import time
+import random
 
 db_coin, db_title = [], []
+count = 1
+
+
+def init_db():
+    pass
+
 
 while True:
     coin, title = crawlsite.get_recent_announced_coin()
-    if db_coin and db_title:
-        if coin == db_coin[len(db_coin)-1] and title == db_title[len(db_title)-1]:
-            print("not updated")
+    if coin == -1 and title == -1:
+        pass
     else:
-        db_coin.append(coin), db_title.append(title)
-        print("{}\n{}".format(coin, title))
+        if db_coin and db_title:
+            if coin == db_coin[len(db_coin)-1] and title == db_title[len(db_title)-1]:
+                print("not updated {}".format(count))
+                count += 1
+        else:
+            db_coin.append(coin), db_title.append(title)
+            print("{}\n{}".format(coin, title))
 
-    time.sleep(1)
+    time.sleep(random.randrange(5, 10))
