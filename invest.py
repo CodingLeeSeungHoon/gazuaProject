179061489a3afbf589e3bf5cf1cb9b71a18df946
CodingLeeSeungHoon <free_minkya@naver.com>
2021-03-29 20:05:24 +0900

    funtion cal_price, solve error, get crawl and buy coin time

--- invest.py
+++ invest.py
@@ -12,16 +12,12 @@ class Investor:
         """
         self.access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
         self.secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
-        self.server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+        self.server_url = "https://api.upbit.com"
         """
 
-        self.access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
-        self.secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
-        self.server_url = "https://api.upbit.com"
+        self.my_account = self.update_account()
 
-        self.my_account = self.my_whole_account()
-
-    def my_whole_account(self):
+    def update_account(self):
         payload = {
             'access_key': self.access_key,
             'nonce': str(uuid.uuid4())
@@ -35,8 +31,23 @@ class Investor:
 
         return res.json()
 
+    @staticmethod
+    def exist_krw(coin_name):
+        url = "https://api.upbit.com/v1/market/all"
+        querystring = {"isDetails": "false"}
+        response = requests.request("GET", url, params=querystring)
+        krw_coin = "KRW-" + coin_name  # COIN_ID
+        check = krw_coin in response.text
+
+        return check
+
     def buy_coin(self, coin):
-        coin_id = 'KRW-' + coin
+        if self.exist_krw(coin):
+            coin_id = 'KRW-' + coin
+        else:
+            print("KRW 코인이 아닙니다. 거래를 중지합니다.")
+            return -1
+
         """
         market : 마켓 ID, 'KRW-***' 형식으로 구성. 필수 기입
         side : 주문 종류, 'bid'는 매수 'ask'는 매도 필수 기입
@@ -45,13 +56,18 @@ class Investor:
         ord_type : 주문타입, 필수, 'limit'은 지정가 주문, 'price'는 시장가 주문 매수, 'market'은 시장가 주문 매도
         """
         whole_krw = -1
-        for a in self.my_account:
-            if a['currency'] == 'KRW':
-                whole_krw = a['balance']
-                break
+        if len(self.my_account) == 1:
+            if self.my_account['currency'] == 'KRW':
+                whole_krw = self.my_account['balance']
+        else:
+            for a in self.my_account:
+                if a['currency'] == 'KRW':
+                    whole_krw = a['balance']
+                    break
 
         # 5000원 이하 금액을 주문하면 오류.
         if float(whole_krw) < 5000:
+            print("보유 KRW 5000원 미만, 거래를 중지합니다.")
             return -1
 
         query = {
@@ -78,6 +94,7 @@ class Investor:
         headers = {"Authorization": authorize_token}
 
         res = requests.post(self.server_url + "/v1/orders", params=query, headers=headers)
+        self.my_account = self.update_account()
         return res
 
 
@@ -103,7 +120,7 @@ class Investor:
         headers = {"Authorization": authorize_token}
 
         res = requests.get(self.server_url + "/v1/order", params=query, headers=headers)
-        remaining_volume = res["remaining_volume"]
+        remaining_volume = res.json()["remaining_volume"]
         return float(remaining_volume)
 
 
@@ -166,11 +183,55 @@ class Investor:
         if "error" not in res:
             print("Sell_market Order Success")
         else:
-            self.sell_market_krw(self, coin_id, volume)
+            self.sell_coin_market(coin_id, volume)
 
     @staticmethod
-    def _cal_price():
-        pass
+    def _cal_price(price):
+        # price == float
+        limit_price = price * 1.1
+        if limit_price >= 2000000:
+            changes = limit_price % 1000
+            return str(limit_price - changes)
+
+        elif limit_price >= 1000000:
+            changes = limit_price % 500
+            return str(limit_price - changes)
+
+        elif limit_price >= 500000:
+            changes = limit_price % 100
+            return str(limit_price - changes)
+
+        elif limit_price >= 100000:
+            changes = limit_price % 50
+            return str(limit_price - changes)
+
+        elif limit_price >= 10000:
+            changes = limit_price % 10
+            return str(limit_price - changes)
+
+        elif limit_price >= 1000:
+            changes = limit_price % 5
+            return str(limit_price - changes)
+
+        elif limit_price >= 100:
+            changes = limit_price % 1
+            return str(limit_price - changes)
+
+        elif limit_price >= 10:
+            tmp = round(limit_price, 1)
+            if tmp > limit_price:
+                return str(tmp - 0.1)
+            else:
+                return str(tmp)
+
+        else:
+            tmp = round(limit_price, 2)
+            if tmp > limit_price:
+                return str(tmp - 0.01)
+            else:
+                return str(tmp)
+
+
 
     def sell_coin_limit(self, resp):
         """
@@ -184,19 +245,20 @@ class Investor:
           ...
         }
         """
-        if "error" not in resp:
+        response = resp.json()
+        if "error" not in response:
             # resp 200 Success
-            order_uuid = resp['uuid']
-            bought_price = int(resp['price'])
-            coin_id = resp['market']
-            coin_volume = resp['volume']
+            order_uuid = response['uuid']
+            bought_price = float(response['price'])
+            coin_id = response['market']
+            coin_volume = response['volume']
 
             # 구매한 코인을 매도, 구매한 코인의 양 만큼 구매가격의 10% 인상시 매도
             query = {
                 'market': coin_id,
                 'side': 'ask',
                 'volume': coin_volume,
-                'price': str(bought_price * 1.1),
+                'price': self._cal_price(bought_price),
                 'ord_type': 'limit',
             }
             query_string = urlencode(query).encode()
@@ -217,19 +279,20 @@ class Investor:
             headers = {"Authorization": authorize_token}
 
             res = requests.post(self.server_url + "/v1/orders", params=query, headers=headers)
-            if "error" not in res:
-                # wait K minutes and check coins are sold
-                time.sleep(10000)
-                not_sold = self.is_not_sold(res['uuid'])
+            if "error" not in res.json():
+                # wait 5 minutes and check coins are sold
+                time.sleep(300)
+                not_sold = self.is_not_sold(res.json()['uuid'])
                 if not_sold:
                     # K초가 지나도 팔리지 않은 경우, 취소 및 시장가로 매도
                     self.order_cancel(order_uuid)
                     self.sell_coin_market(coin_id, not_sold)
                     print("sell {} coin market price.".format(coin_id.replace("KRW-", "")))
+                    self.my_account = self.update_account()
                 else:
                     # 판매 완료 MSG, Investor 계좌 갱신
                     print("Success sell {} coin!".format(coin_id.replace("KRW-", "")))
-                    self.my_account = self.my_whole_account()
+                    self.my_account = self.update_account()
 
             else:
                 # res 400 Failed
--- main.py
+++ main.py
@@ -25,13 +25,15 @@ while True:
     else:
         if db_coin and db_title and coin == db_coin[len(db_coin) - 1] and title == db_title[len(db_title) - 1]:
             # least one data in db, not updated new coin announcement
-            print("not updated {}".format(count))
+            print("not updated {}, coin_db = {}".format(count, db_coin))
             count += 1
         else:
             # no data in db or new coin announcement updated!
             db_coin.append(coin), db_title.append(title)
+            now = time.localtime()
             print("{}\n{}".format(coin, title))
-
+            print("{}/{}/{} {}:{}:{}".format(now.tm_year, now.tm_mon, now.tm_mday,
+                                             now.tm_hour, now.tm_min, now.tm_sec))
             # investor.buy_coin(coin)
             resp = investor.buy_coin(coin)
             if resp != -1:
